0e7cc8ad828daa12094e9aa0eebd6fd3ad7ddf1f
Barkat Ali <barkatali8016@gmail.com>
2022-09-15 15:12:01 +0530

    added swagger is implemented

--- src/controllers/controller.test.js
+++ src/controllers/controller.test.js
@@ -4,6 +4,7 @@ const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 const sinon = require("sinon");
 const sinonChai = require("sinon-chai");
+const { default: axios } = require("axios");
 chai.use(sinonChai);
 const rewire = require("rewire");
 var controller = rewire("./controller.js");
@@ -63,26 +64,62 @@ describe("controller", () => {
       // expect(response.status).to.be.equal(200);
     });
   });
-  context("getNearBySuperMarkets", () => {
-    it("should call getNearBySuperMarkets and return formatedResponse", () => {
-      const mockReq = {
-        params: { postcode: 700001 },
-        query: {
-          limit: 20,
-          radius: 5000,
-        },
+
+  context("formatResponse", () => {
+    it("should call formatResponse", () => {
+      const mockParams = {
+        data: { features: [{ pro: 122 }] },
       };
-      const mockRes = {};
-      const result = controller.getNearBySuperMarkets(mockReq, mockRes);
-      expect(result).to.be.exist;
+      const response = controller.__get__("formatResponse")(mockParams);
+      expect(response).to.be.a("object");
+      expect(response.total).to.be.equal(1);
     });
 
-    it("should fetch nearest super makets  based on post code", () => {
+    it("should call formatResponse No details found for this postcode!", () => {
+      const mockParams = {
+        data: { features: [] },
+      };
+      const response = controller.__get__("formatResponse")(mockParams);
+      expect(response).to.be.a("object");
+      expect(response.message)
+        .to.be.a("string")
+        .to.be.equal("No details found for this postcode!");
+    });
+  });
+
+  context("getNearBySuperMarkets", () => {
+    // it("should call getNearBySuperMarkets and return formatedResponse", () => {
+    //   const mockReq = {
+    //     params: { postcode: 700001 },
+    //     query: {
+    //       limit: 20,
+    //       radius: 5000,
+    //     },
+    //   };
+    //   const mockRes = {};
+    //   const result = controller.getNearBySuperMarkets(mockReq, mockRes);
+    //   expect(result).to.be.exist;
+    // });
+
+    it("should call getNearBySuperMarkets based on post code", async () => {
       const mockResponse = {
         data: { results: [{ lat: 123, lon: -123 }] },
       };
       sandbox.stub(utils, "getLatLongByPostCode").resolves(mockResponse);
 
+      const mockResponse2 = {
+        data: { features: [{ pro: 122 }] },
+      };
+      // const fx = controller.__get__("getSuperMarketsHelper");
+      // sandbox.stub(controller, "formatResponse").resolves(mockResponse2);
+      let formatResponseStub = sinon.stub().resolves(mockResponse2);
+
+      controller.__set__("formatResponse", formatResponseStub);
+      const mockRes1 = {
+        data: { features: [{ pro: 122 }] },
+      };
+      sandbox.stub(axios, "get").resolves(mockResponse);
+
       const mockReq = {
         params: { postcode: 700001 },
         query: {
@@ -91,13 +128,13 @@ describe("controller", () => {
         },
       };
       const mockRes = {};
-      const result = controller.getNearBySuperMarkets;
+      const result = await controller.getNearBySuperMarkets(mockReq, mockRes);
 
-      try {
-        result(mockReq, mockRes);
-      } catch (error) {
-        expect(error).to.be.equal(err);
-      }
+      // try {
+      //   result(mockReq, mockRes);
+      // } catch (error) {
+      // }
+      expect(result).to.be.equal(mockResponse);
     });
 
     it("should fetch nearest super makets  based on post code and throw error", () => {
